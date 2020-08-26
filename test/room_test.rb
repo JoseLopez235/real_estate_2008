403a474b838dc10f1f4f648da54db8269798b52d
Jose Lopez <joselopez11394@gmail.com>
2020-08-26 11:09:52 -0400

    adds test for if room is painted

--- test/room_test.rb
+++ test/room_test.rb
@@ -19,4 +19,11 @@ class RoomTest < Minitest::Test
     assert_equal 130, room1.area
     assert_equal 180, room2.area
   end
+
+  def test_should_return_boolean_if_painted_or_not
+    room = Room.new(:bedroom, 10, '13')
+    assert_equal false, room.is_painted?
+    room.paint
+    assert_equal true, room.is_painted?
+  end
 end
