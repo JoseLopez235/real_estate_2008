e85688c4e6201f7d7db4b692409180e5eda60338
Jose Lopez <joselopez11394@gmail.com>
2020-08-26 11:22:32 -0400

    adds test to check if it can add rooms to romms array

--- test/house_test.rb
+++ test/house_test.rb
@@ -1,6 +1,7 @@
 require "minitest/autorun"
 require "minitest/pride"
 require "./lib/house"
+require "./lib/room"
 
 class HouseTest < Minitest::Test
   def test_should_exists
@@ -12,5 +13,19 @@ class HouseTest < Minitest::Test
     house = House.new("$400000", "123 sugar lane")
     assert_equal "$400000", house.price
     assert_equal "123 sugar lane", house.address
+  end
+
+  def test_should_return_an_empty_array_of_rooms
+    house = House.new("$400000", "123 sugar lane")
+    assert_equal [], house.rooms
+  end
+
+  def test_should_add_a_room_to_the_rooms_array
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    assert_equal [room_1,room_2], house.rooms
   end
 end
